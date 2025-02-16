31b3f3b30eecafc38b0b45577d6d1ab6f6f9ce4b
Alex62-Creator <cham62ss@gmail.com>
2025-02-16 18:16:36 +0300

    add all page and save

--- Lesson_PS05_Scrapy/Lesson_PS05_Scrapy/spiders/divanpars.py
+++ Lesson_PS05_Scrapy/Lesson_PS05_Scrapy/spiders/divanpars.py
@@ -5,11 +5,15 @@ class DivanparsSpider(scrapy.Spider):
     name = "divanpars"
     allowed_domains = ["https://divan.ru"]
     start_urls = ["https://divan.ru/category/divany-i-kresla"]
+    for i in ('2', '3', '4', '5'):
+        url = 'https://www.divan.ru/category/divany-i-kresla/page-' + i
+        start_urls.append(url)
+    print(start_urls)
 
     def parse(self, response):
         # Создаём переменную, в которую будет сохраняться информация
         # Пишем ту же команду, которую писали в терминале
-        divans = response.css('div._Ud0k')
+        divans = response.css('div.lsooF')
         # Настраиваем работу с каждым отдельным диваном в списке
         for divan in divans:
             # Используем новый для нас оператор "yield", который помогает обрабатывать одно отдельное действие
@@ -18,10 +22,12 @@ class DivanparsSpider(scrapy.Spider):
             yield {
                 # Ссылки и теги получаем с помощью консоли на сайте
                 # Создаём словарик названий, используем поиск по диву, а внутри дива — по тегу span
-                'name': divan.css('div.lsooF span::text').get(),
+                'name': divan.css('span::text').get(),
                 # Создаём словарик цен, используем поиск по диву, а внутри дива — по тегу span
                 'price': divan.css('div.pY3d2 span::text').get(),
                 # Создаём словарик ссылок, используем поиск по тегу "a", а внутри тега — по атрибуту
                 # Атрибуты — это настройки тегов
                 'url': divan.css('a').attrib['href']
             }
+
+
---
+++ Lesson_PS05_Scrapy/Lesson_PS05_Scrapy/spiders/svet.jl
@@ -0,0 +1,190 @@
+{"name": "Люстра Ливсден Brown", "price": "16 990", "url": "/product/lyustra-livsden-brown"}
+{"name": "Подвесной светильник Ливсден Brown", "price": "4590", "url": "/product/podvesnoj-svetilnik-livsden-brown"}
+{"name": "Настольная лампа Ливсден Brown", "price": "7590", "url": "/product/nastolnaya-lampa-livsden-brown"}
+{"name": "Торшер Ливсден Brown", "price": "11 990", "url": "/product/torsher-livsden-brown"}
+{"name": "Бра Флиммер Black", "price": "1690", "url": "/product/bra-flimmer-black"}
+{"name": "Люстра Флиммер Black", "price": "6990", "url": "/product/lyustra-flimmer-black"}
+{"name": "Подвесной светильник Флиммер Black", "price": "1790", "url": "/product/podvesnoj-svetilnik-flimmer-black"}
+{"name": "Настольная лампа Флиммер Black", "price": "2990", "url": "/product/nastolnaya-lampa-flimmer-black"}
+{"name": "Торшер Флиммер Black", "price": "4990", "url": "/product/torsher-flimmer-black"}
+{"name": "Настольная лампа Нидлс Raffia Beige", "price": "4990", "url": "/product/nastolnaya-lampa-nidls-raffia-beige"}
+{"name": "Люстра Гейза Black", "price": "8990", "url": "/product/lyustra-gejza-black"}
+{"name": "Настольная лампа Гейза Black", "price": "3990", "url": "/product/nastolnaya-lampa-gejza-black"}
+{"name": "Настольная лампа Гейза Pink", "price": "2590", "url": "/product/nastolnaya-lampa-gejza-pink"}
+{"name": "Настольная лампа Гейза Blue", "price": "3990", "url": "/product/nastolnaya-lampa-gejza-blue"}
+{"name": "Настольная лампа Гейза Orange", "price": "2590", "url": "/product/nastolnaya-lampa-gejza-orange"}
+{"name": "Настольная лампа Алней Black", "price": "3990", "url": "/product/nastolnaya-lampa-alnej-black"}
+{"name": "Настольная лампа Алней Beige", "price": "3990", "url": "/product/nastolnaya-lampa-alnej-beige"}
+{"name": "Настольная лампа Алней Green", "price": "3990", "url": "/product/nastolnaya-lampa-alnej-green"}
+{"name": "Настольная лампа Алней Orange", "price": "3990", "url": "/product/nastolnaya-lampa-alnej-orange"}
+{"name": "Торшер Алней Green", "price": "7990", "url": "/product/torsher-alnej-green"}
+{"name": "Торшер Алней Black", "price": "7990", "url": "/product/torsher-alnej-black"}
+{"name": "Торшер Алней Orange", "price": "7990", "url": "/product/torsher-alnej-orange"}
+{"name": "Торшер Алней Beige", "price": "7990", "url": "/product/torsher-alnej-beige"}
+{"name": "Настольная лампа Арбет Orange", "price": "2390", "url": "/product/nastolnaya-lampa-arbet-orange"}
+{"name": "Настольная лампа Арбет Silver", "price": "2590", "url": "/product/nastolnaya-lampa-arbet-silver"}
+{"name": "Настольная лампа Арбет Beige", "price": "1890", "url": "/product/nastolnaya-lampa-arbet-beige"}
+{"name": "Настольная лампа Арбет White", "price": "1890", "url": "/product/nastolnaya-lampa-arbet-white"}
+{"name": "Настольная лампа Арбет Black", "price": "1890", "url": "/product/nastolnaya-lampa-arbet-black"}
+{"name": "Настольная лампа Синдри Grey", "price": "4390", "url": "/product/nastolnaya-lampa-sindri-grey"}
+{"name": "Люстра Синдри Black", "price": "13 990", "url": "/product/lyustra-sindri-black"}
+{"name": "Настольная лампа Манэт White", "price": "2290", "url": "/product/nastolnaya-lampa-manet-white"}
+{"name": "Настольная лампа Леррет Beige", "price": "3990", "url": "/product/nastolnaya-lampa-lerret-beige"}
+{"name": "Настольная лампа Леррет White", "price": "2590", "url": "/product/nastolnaya-lampa-lerret-white"}
+{"name": "Настольная лампа Истад Black", "price": "1590", "url": "/product/nastolnaya-lampa-istad-black"}
+{"name": "Настольная лампа Истад White", "price": "1590", "url": "/product/nastolnaya-lampa-istad-white"}
+{"name": "Люстра Боллар White", "price": "22 990", "url": "/product/lyustra-bollar-white"}
+{"name": "Настольная лампа Рибе Black", "price": "1890", "url": "/product/nastolnaya-lampa-ribe-black"}
+{"name": "Настольная лампа Рибе Orange", "price": "1890", "url": "/product/nastolnaya-lampa-ribe-orange"}
+{"name": "Настольная лампа Снибальд White", "price": "490", "url": "/product/nastolnaya-lampa-snibald-white"}
+{"name": "Настольная лампа Спайк Pink", "price": "1290", "url": "/product/nastolnaya-lampa-spajk-pink"}
+{"name": "Настольная лампа Спайк White", "price": "1290", "url": "/product/nastolnaya-lampa-spajk-white"}
+{"name": "Настольная лампа Астри Black", "price": "1290", "url": "/product/nastolnaya-lampa-astri-black"}
+{"name": "Настольная лампа Астри Beige", "price": "1390", "url": "/product/nastolnaya-lampa-astri-beige"}
+{"name": "Подвесной светильник Клауд White", "price": "10 990", "url": "/product/podvesnoj-svetilnik-klaud-white"}
+{"name": "Торшер Клауд White", "price": "18 290", "url": "/product/torsher-klaud-white"}
+{"name": "Настольная лампа Клауд White", "price": "15 990", "url": "/product/nastolnaya-lampa-klaud-white"}
+{"name": "Настольная лампа Машрум Olive", "price": "3990", "url": "/product/nastolnaya-lampa-mashrum-olive"}
+{"name": "Настольная лампа Машрум Pink", "price": "3990", "url": "/product/nastolnaya-lampa-mashrum-pink"}
+{"name": "Подвесной светильник Бабл Quartz", "price": "11 490", "url": "/product/podvesnoj-svetilnik-babl-quartz"}
+{"name": "Подвесной светильник Рэн Gray", "price": "8990", "url": "/product/podvesnoj-svetilnik-ren-gray"}
+{"name": "Подвесной светильник Пескара Black", "price": "7190", "url": "/product/podvesnoj-svetilnik-peskara-black"}
+{"name": "Подвесной светильник Лечче Rose", "price": "5090", "url": "/product/podvesnoj-svetilnik-lechche-rose"}
+{"name": "Подвесной светильник Рэн Beige", "price": "8990", "url": "/product/podvesnoj-svetilnik-ren-beige"}
+{"name": "Настольная лампа Флинт White", "price": "6990", "url": "/product/torsher-flint-white"}
+{"name": "Торшер Флейк White", "price": "11 990", "url": "/product/torsher-flejk-white"}
+{"name": "Подвесной светильник Плейн White", "price": "10 990", "url": "/product/podvesnoj-svetilnik-plejn-white"}
+{"name": "Подвесной светильник Лечче Mint", "price": "5590", "url": "/product/podvesnoj-svetilnik-lechche-mint"}
+{"name": "Подвесной светильник Дроп Black", "price": "2790", "url": "/product/podvesnoj-svetilnik-drop-black"}
+{"name": "Подвесной светильник Лума Black", "price": "4090", "url": "/product/podvesnoj-svetilnik-luma-black"}
+{"name": "Подвесной светильник Фиона White", "price": "2990", "url": "/product/podvesnoj-svetilnik-fiona-white"}
+{"name": "Подвесной светильник Селф Black", "price": "4190", "url": "/product/podvesnoj-svetilnik-self-black"}
+{"name": "Подвесной светильник Ринна Black", "price": "5290", "url": "/product/podvesnoj-svetilnik-rinna-black"}
+{"name": "Настольная лампа Отто Black", "price": "4490", "url": "/product/nastolnaya-lampa-otto-black"}
+{"name": "Подвесной светильник Альта White", "price": "3990", "url": "/product/podvesnoj-svetilnik-alta-white"}
+{"name": "Подвесной светильник Мантис Brown", "price": "17 990", "url": "/product/podvesnoj-svetilnik-mantis-brown"}
+{"name": "Подвесной светильник Лума White", "price": "4190", "url": "/product/podvesnoj-svetilnik-luma-white"}
+{"name": "Торшер Отто Black", "price": "7590", "url": "/product/torsher-otto-black"}
+{"name": "Торшер Флинт-1 White", "price": "17 290", "url": "/product/torsher-flint-1-white"}
+{"name": "Бра Техно Стрикт-1 Black", "price": "3590", "url": "/product/bra-tehno-strikt-1-black"}
+{"name": "Подвесной светильник Портланд Black", "price": "3590", "url": "/product/podvesnoj-svetilnik-portland-black"}
+{"name": "Подвесной светильник Ньютаун-1 Black", "price": "11 490", "url": "/product/podvesnoj-svetilnik-nyutaun-1-black"}
+{"name": "Торшер Осло White", "price": "16 990", "url": "/product/torsher-oslo-white"}
+{"name": "Подвесной светильник Кая White", "price": "2990", "url": "/product/podvesnoj-svetilnik-kaya-white"}
+{"name": "Настольная лампа Машрум Grey", "price": "4590", "url": "/product/nastolnaya-lampa-mashrum-grey"}
+{"name": "Торшер Рэй Black", "price": "5290", "url": "/product/torsher-rej-black"}
+{"name": "Торшер Гросс White", "price": "7990", "url": "/product/torsher-gross-white"}
+{"name": "Торшер Вена Beige", "price": "11 990", "url": "/product/torsher-vena-beige"}
+{"name": "Торшер Рэй White", "price": "5590", "url": "/product/torsher-rej-white"}
+{"name": "Настольная лампа Сфера Black", "price": "5390", "url": "/product/nastolnaya-lampa-sfera-black"}
+{"name": "Подвесной светильник Пескара Green", "price": "5990", "url": "/product/podvesnoj-svetilnik-peskara-green"}
+{"name": "Торшер Moderli Floor White", "price": "6990", "url": "/product/torsher-moderli-floor-white"}
+{"name": "Торшер Варио Black", "price": "7590", "url": "/product/torsher-vario-black"}
+{"name": "Настольная лампа Флейк White", "price": "6990", "url": "/product/nastolnaya-lampa-flejk-white"}
+{"name": "Подвесной светильник Флейк White", "price": "5590", "url": "/product/podvesnoj-svetilnik-flejk-white"}
+{"name": "Торшер Шоу Black", "price": "10 990", "url": "/product/torsher-shou-black"}
+{"name": "Торшер Нидлс Khaki", "price": "14 990", "url": "/product/torsher-nidls-khaki"}
+{"name": "Торшер Грено Black", "price": "11 990", "url": "/product/torsher-greno-black"}
+{"name": "Настольная лампа Рэй Black", "price": "3590", "url": "/product/nastolnaya-lampa-rej-black"}
+{"name": "Настольная лампа Раффи Round", "price": "7190", "url": "/product/nastolnaya-lampa-raffi-round"}
+{"name": "Торшер Билли Black", "price": "19 690", "url": "/product/torsher-billi-black"}
+{"name": "Настольная лампа Гровер Glass", "price": "6490", "url": "/product/nastolnaya-lampa-grover-glass"}
+{"name": "Настольная лампа Рэй White", "price": "3590", "url": "/product/nastolnaya-lampa-rej-white"}
+{"name": "Подвесной светильник Плейн Black", "price": "11 990", "url": "/product/podvesnoj-svetilnik-plejn-black"}
+{"name": "Торшер Вена Black", "price": "11 990", "url": "/product/torsher-vena-black"}
+{"name": "Настольная лампа Сфера Orange", "price": "5390", "url": "/product/nastolnaya-lampa-sfera-orange"}
+{"name": "Настольная лампа Спайк Orange", "price": "990", "url": "/product/nastolnaya-lampa-spajk-orange"}
+{"name": "Торшер Портланд Black", "price": "7590", "url": "/product/torsher-portland-black"}
+{"name": "Торшер Arco", "price": "13 140", "url": "/product/torsher-arco"}
+{"name": "Торшер Duetto-1", "price": "5190", "url": "/product/torsher-duetto-1"}
+{"name": "Спот Лима-3 Black", "price": "7790", "url": "/product/spot-lima-3-black"}
+{"name": "Настольная лампа Машрум Gold", "price": "4990", "url": "/product/nastolnaya-lampa-mashrum-gold"}
+{"name": "Бра Берит Black", "price": "3990", "url": "/product/bra-berit-black"}
+{"name": "Бра Берит Orange", "price": "3990", "url": "/product/bra-berit-orange"}
+{"name": "Бра Бракетт Black", "price": "3990", "url": "/product/bra-brakett-black"}
+{"name": "Бра Бракетт White", "price": "3990", "url": "/product/bra-brakett-white"}
+{"name": "Подвесной светильник Малинг Olive", "price": "5590", "url": "/product/podvesnoj-svetilnik-maling-olive"}
+{"name": "Подвесной светильник Малинг Black", "price": "5590", "url": "/product/podvesnoj-svetilnik-maling-black"}
+{"name": "Подвесной светильник Малинг Beige", "price": "5590", "url": "/product/podvesnoj-svetilnik-maling-beige"}
+{"name": "Люстра Малинг Multi", "price": "14 990", "url": "/product/lyustra-maling-multi"}
+{"name": "Торшер Осло Blue", "price": "17 990", "url": "/product/torsher-oslo-blue"}
+{"name": "Торшер Осло Black", "price": "16 990", "url": "/product/torsher-oslo-black"}
+{"name": "Торшер Энкель Black", "price": "5590", "url": "/product/torsher-enkel-black"}
+{"name": "Настольная лампа Требен Orange", "price": "2590", "url": "/product/nastolnaya-lampa-treben-orange"}
+{"name": "Подвесной светильник Сансет Black", "price": "4590", "url": "/product/podvesnoj-svetilnik-sanset-black"}
+{"name": "Торшер Раффи Round", "price": "11 690", "url": "/product/torsher-raffi-round"}
+{"name": "Настольная лампа Гросс White", "price": "4690", "url": "/product/nastolnaya-lampa-gross-white"}
+{"name": "Настольная лампа Сканди White", "price": "4690", "url": "/product/nastolnaya-lampa-skandi-white"}
+{"name": "Торшер Сканди White", "price": "9990", "url": "/product/torsher-skandi-white"}
+{"name": "Торшер Бабл-1", "price": "7990", "url": "/product/torsher-babl-1"}
+{"name": "Подвесной светильник Бабл-3", "price": "5990", "url": "/product/podvesnoj-svetilnik-babl-3"}
+{"name": "Бра Бабл Black", "price": "2490", "url": "/product/bra-babl-black"}
+{"name": "Настольная лампа Дэнди Black", "price": "5990", "url": "/product/nastolnaya-lampa-dendi-black"}
+{"name": "Настольная лампа Дэнди White", "price": "5990", "url": "/product/nastolnaya-lampa-dendi-white"}
+{"name": "Торшер Варио White", "price": "7590", "url": "/product/torsher-vario-white"}
+{"name": "Бра Варио Black", "price": "1490", "url": "/product/bra-vario-black"}
+{"name": "Бра Варио White", "price": "1490", "url": "/product/bra-vario-white"}
+{"name": "Подвесной светильник Сансет White", "price": "4590", "url": "/product/podvesnoj-svetilnik-sanset-white"}
+{"name": "Подвесной светильник Кэндел Black", "price": "3990", "url": "/product/podvesnoj-svetilnik-kendel-black"}
+{"name": "Подвесной светильник Кэндел White", "price": "3990", "url": "/product/podvesnoj-svetilnik-kendel-white"}
+{"name": "Торшер Мун Black", "price": "8990", "url": "/product/torsher-mun-black"}
+{"name": "Бра Moderli Alina", "price": "2990", "url": "/product/bra-moderli-alina"}
+{"name": "Подвесной светильник Маро Green", "price": "7590", "url": "/product/podvesnoj-svetilnik-maro-green"}
+{"name": "Светильник потолочный Matthew", "price": "5690", "url": "/product/svetilnik-potolochnyj-matthew"}
+{"name": "Бра Galdakao-1", "price": "6590", "url": "/product/bra-galdakao"}
+{"name": "Подвесной светильник Лума Gold", "price": "4990", "url": "/product/podvesnoj-svetilnik-luma-gold"}
+{"name": "Люстра Монти-5 White", "price": "17 190", "url": "/product/lyustra-monti-5-white"}
+{"name": "Торшер Билли White", "price": "19 690", "url": "/product/torsher-billi-white"}
+{"name": "Подвесной светильник Ори Gold", "price": "3090", "url": "/product/podvesnoj-svetilnik-ori-gold"}
+{"name": "Бра Bolla-Unica", "price": "5290", "url": "/product/bra-bolla-unica"}
+{"name": "Подвесной светильник Онтарио Olive", "price": "10 490", "url": "/product/podvesnoj-svetilnik-ontario-olive"}
+{"name": "Подвесной светильник Онтарио Brown", "price": "12 390", "url": "/product/podvesnoj-svetilnik-ontario-brown"}
+{"name": "Настольная лампа Moderli Suspent", "price": "3890", "url": "/product/nastolnaya-lampa-moderli-suspent"}
+{"name": "Подвесной светильник Онтарио Gray", "price": "12 390", "url": "/product/podvesnoj-svetilnik-ontario-gray"}
+{"name": "Торшер Moderli Suspent", "price": "7590", "url": "/product/torsher-moderli-suspent"}
+{"name": "Подвесной светильник Альта Black", "price": "4290", "url": "/product/podvesnoj-svetilnik-alta-black"}
+{"name": "Подвесной светильник Narices-1", "price": "30 290", "url": "/product/podvesnoj-svetilnik-narices-1"}
+{"name": "Подвесной светильник Селф Gold", "price": "4390", "url": "/product/podvesnoj-svetilnik-self-gold"}
+{"name": "Подвесной светильник Кая Black", "price": "2890", "url": "/product/podvesnoj-svetilnik-kaya-black"}
+{"name": "Подвесной светильник Эйр White", "price": "6290", "url": "/product/podvesnoj-svetilnik-ejr-white"}
+{"name": "Подвесной светильник Селф White", "price": "4090", "url": "/product/podvesnoj-svetilnik-self-white"}
+{"name": "Подвесной светильник Moderli Lewes", "price": "14 690", "url": "/product/podvesnoj-svetilnik-moderli-lewes"}
+{"name": "Подвесной светильник Клэри Brown", "price": "9490", "url": "/product/podvesnoj-svetilnik-kleri-brown"}
+{"name": "Подвесной светильник Вэлли Gray", "price": "6490", "url": "/product/podvesnoj-svetilnik-velli-gray"}
+{"name": "Бра Равелло Black", "price": "5890", "url": "/product/bra-ravello-black"}
+{"name": "Подвесной светильник Талли Black", "price": "7490", "url": "/product/podvesnoj-svetilnik-talli-black"}
+{"name": "Спот Лайт-1 White", "price": "2890", "url": "/product/spot-lajt-1-white"}
+{"name": "Подвесной светильник Вэлли Olive", "price": "6490", "url": "/product/podvesnoj-svetilnik-velli-olive"}
+{"name": "Бра Nevy Silver", "price": "8690", "url": "/product/bra-nevy-silver"}
+{"name": "Подвесной светильник Агато Black", "price": "9490", "url": "/product/podvesnoj-svetilnik-agato-black"}
+{"name": "Подвесной светильник Priddy", "price": "9890", "url": "/product/podvesnoj-svetilnik-priddy"}
+{"name": "Бра Твинки Black", "price": "3690", "url": "/product/bra-tvinki-black"}
+{"name": "Подвесной светильник Дроп Beige", "price": "3490", "url": "/product/podvesnoj-svetilnik-drop-beige"}
+{"name": "Подвесной светильник Фиона Black", "price": "2990", "url": "/product/podvesnoj-svetilnik-fiona-black"}
+{"name": "Настольная лампа Омаха White", "price": "10 490", "url": "/product/nastolnaya-lampa-omaha-white"}
+{"name": "Подвесной светильник Агато Brown", "price": "9490", "url": "/product/podvesnoj-svetilnik-agato-brown"}
+{"name": "Подвесной светильник Moderli Host-1", "price": "4390", "url": "/product/podvesnoj-svetilnik-moderli-host-1"}
+{"name": "Подвесной светильник Ори White", "price": "2990", "url": "/product/podvesnoj-svetilnik-ori-white"}
+{"name": "Люстра Джеоми White", "price": "13 890", "url": "/product/lyustra-djeomi-white"}
+{"name": "Подвесной светильник Селф Gray", "price": "3990", "url": "/product/podvesnoj-svetilnik-self-gray"}
+{"name": "Подвесной светильник Ринна White", "price": "5590", "url": "/product/podvesnoj-svetilnik-rinna-white"}
+{"name": "Бра Леон Black", "price": "2390", "url": "/product/bra-leon-black"}
+{"name": "Подвесной светильник Эйр Brown", "price": "5990", "url": "/product/podvesnoj-svetilnik-ejr-brown"}
+{"name": "Подвесной светильник Ридж White", "price": "3290", "url": "/product/podvesnoj-svetilnik-ridj-white"}
+{"name": "Подвесной светильник Дроп Gray", "price": "2990", "url": "/product/podvesnoj-svetilnik-drop-gray"}
+{"name": "Люстра Лима-5 White", "price": "9790", "url": "/product/lyustra-lima-5-white"}
+{"name": "Бра Брик Black", "price": "4190", "url": "/product/bra-brik-black"}
+{"name": "Бра Твинки Grey", "price": "3690", "url": "/product/bra-tvinki-grey"}
+{"name": "Настольная лампа Шоу Black", "price": "7190", "url": "/product/nastolnaya-lampa-shou-black"}
+{"name": "Подвесной светильник Мантис Olive", "price": "17 990", "url": "/product/podvesnoj-svetilnik-mantis-olive"}
+{"name": "Торшер Омаха White", "price": "12 490", "url": "/product/torsher-omaha-white"}
+{"name": "Подвесной светильник Онтарио Black", "price": "10 490", "url": "/product/podvesnoj-svetilnik-ontario-black"}
+{"name": "Подвесной светильник Пескара Yellow", "price": "5590", "url": "/product/podvesnoj-svetilnik-peskara-yellow"}
+{"name": "Подвесной светильник Дроп White", "price": "2990", "url": "/product/podvesnoj-svetilnik-drop-white"}
+{"name": "Подвесной светильник Ринна Beige", "price": "5590", "url": "/product/podvesnoj-svetilnik-rinna-beige"}
+{"name": "Подвесной светильник Ори Black", "price": "3090", "url": "/product/podvesnoj-svetilnik-ori-black"}
+{"name": "Подвесной светильник Фиона Gray", "price": "3090", "url": "/product/podvesnoj-svetilnik-fiona-gray"}
+{"name": "Подвесной светильник Эйр Beige", "price": "5990", "url": "/product/podvesnoj-svetilnik-ejr-beige"}
+{"name": "Подвесной светильник Клэри BLack", "price": "9490", "url": "/product/podvesnoj-svetilnik-kleri-black"}
+{"name": "Подвесной светильник Альта Gold", "price": "4290", "url": "/product/podvesnoj-svetilnik-alta-gold"}
+{"name": "Подвесной светильник Мантис Black", "price": "17 990", "url": "/product/podvesnoj-svetilnik-mantis-black"}
